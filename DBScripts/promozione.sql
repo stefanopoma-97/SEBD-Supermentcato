3d2e5d22a2bee9eed3a3b21cf5837ed940874d71
Stefano <stefano1997poma97@gmail.com>
2021-12-04 12:22:45 +0100

    creato home page visitatore

---
+++ DBScripts/promozione.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO `promozione` (`oid`, `sconto`, `scadenza`, `valida_in_negozio`, `valida_online`, `negozio_oid`, `prodotto_oid`) VALUES
+(1, '10.00', '2022-01-31', b'1', b'0', 1, 1),
+(2, '5.00', '2022-01-31', b'0', b'1', 1, 2);
+
+--
+-- Indici per le tabelle scaricate
+--
+
